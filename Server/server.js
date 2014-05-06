7d9b7b38fd4d662acf9d39edd2c6defeacdc29ab
pallab-gain <pallab.gain@gmail.com>
2014-05-07 00:14:09 +0600

    heruko'te up korchi server code'ta

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@
 (function () {
     // this should really be in a config file!
     var conf = {
-        client_id: '1429906980595399', client_secret: 'af662fb9b3f927e7549f72c9669d752d', scope: 'email, user_about_me, user_birthday', redirect_uri: 'http://localhost:3000/fblogin'
+        client_id: '1429906980595399', client_secret: 'af662fb9b3f927e7549f72c9669d752d', scope: 'email, user_about_me, user_birthday', redirect_uri: 'http://tabstart.herokuapp.com/fblogin'
     };
 
     var express, app, http, route, _, bodyParser, graph;
--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var current_active = undefined;
 var xhr = new XMLHttpRequest();
-var successURL = 'http://localhost:3000/loginsuccess';
+var successURL = 'http://tabstart.herokuapp.com/loginsuccess';
 
 function get_time(gg) {
     var mn = 60, hr = mn * 60, dy = hr * 24, yr = dy * 365, tmp;
@@ -119,7 +119,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, cur_tab) {
                 (function (ctab) {
                     if (ctab.url.indexOf(successURL) === 0) {
                         chrome.tabs.remove(ctab.id);
-                        xhr.open("GET", "http://localhost:3000/getdata", true);
+                        xhr.open("GET", "http://tabstart.herokuapp.com/getdata", true);
                         xhr.send();
                         //localStorage.setItem('accessToken',access);
                     }
@@ -140,7 +140,7 @@ xhr.onreadystatechange = function () {
             response = response.response;
             if(response && response.id){
                 localStorage.setItem('tSfbid', response.id);
-                //console.log( response);
+                console.log( response);
             }
         })(JSON.parse(xhr.responseText))
     }
